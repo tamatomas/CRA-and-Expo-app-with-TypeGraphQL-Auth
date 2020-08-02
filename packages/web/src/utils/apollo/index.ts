76d3486829ad861465b7e35c1400ddebe779825f
Tomas Tamagnone <tomastamagnone.lamarti@gmail.com>
2020-08-02 19:43:07 -0300

    react web env vars

--- packages/web/src/utils/apollo/index.ts
+++ packages/web/src/utils/apollo/index.ts
@@ -1,7 +1,8 @@
 import { ApolloClient } from '@apollo/client';
 import { cache } from './cache';
 
-const uri = (process.env.NODE_ENV === "production") ? process.env.APOLLO_URL : 'http://localhost:4000/graphql'
+console.log(process.env.APOLLO_URL);
+const uri = (process.env.NODE_ENV === "production") ? "https://testingupdateoptions.herokuapp.com/graphql" : 'http://localhost:4000/graphql'
 
 export const apolloClient = new ApolloClient({
   uri,
